7c5c679e65b5fa88f59c38c77d171c8463b3cd53
nkiyo <nkiyo@hoge.com>
2018-07-24 08:49:33 +0900

    sample csv read write

--- csv/csv_sample.py
+++ csv/csv_sample.py
@@ -3,12 +3,22 @@
 
 import csv
 
+# read csv
+# '#'から始まるコメント行を自動的に読込スキップする方法不明
 exampleFile = open('example.csv')
 exampleReader = csv.reader(exampleFile)
-exampleData = list(exampleReader)
-#print(exampleData)
-for line in exampleData:
-    print('line is ' + str(line))
-    for elem in line:
+for row in exampleReader:
+    print('Row #' + str(exampleReader.line_num) + ' is ' + str(row))
+    for elem in row:
         print('elem is ' + elem)
+
+# write csv
+outputFile = open('output.csv', 'w', newline = '')
+outputWriter = csv.writer(outputFile)
+outputWriter.writerow(['spam', 'eggs' , 'bacon', 'ham'])
+outputWriter.writerow(['Hello, world', 'eggs', 'bacon', 'ham'])
+outElem = [1, 2, 3,141592, 4]
+print(str(type(outElem))) # => 'list'
+outputWriter.writerow(outElem)
+outputFile.close()
 
